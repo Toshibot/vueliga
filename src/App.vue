09210010b3d2d1e8526ed68de418d59e2bed3799
Toshi Stewart <toshi@chrono24.com>
2023-08-04 14:02:43 +0200

    Data objects included into the app

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
 
 </style>
 <script lang="ts">
+import axios from 'axios'
 import Matches from "./components/Matches.vue"
 
 export default {
@@ -21,24 +22,28 @@ export default {
   },
   data() {
     return {
-      compData: {},
+      clubData: [],
+      tableData: [],
+      matchData: []
     }
   },
-  methods: {
-    async getData() {
-      try {
-        let response = fetch("https://api.football-data.org/v4/competitions", {
-          mode: "no-cors",
-          credentials: "include"
-        });
-        this.compData = await response.json;
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  },
-  created() {
-    this.getData();
+  mounted() {
+    // Club Data - Hosted
+    axios.get('./data/data-teams.json')
+      .then(response => {
+        console.log(response.data);
+        this.clubData = response.data;
+      })
+    axios.get('./data/table-data.json')
+      .then(response => {
+        console.log(response.data);
+        this.tableData = response.data;
+      })
+    axios.get('./data/match-data.json')
+      .then(response => {
+        console.log(response.data);
+        this.matchData = response.data;
+      })
   }
 }
 </script>
