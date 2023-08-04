afb7f8e99e968ab3caa0bdb18e59e002168bd2ea
Toshi Stewart <toshi@chrono24.com>
2023-08-04 15:18:50 +0200

    Match component created

---
+++ src/components/matches/Match.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span>{{ homeTeam }}</span> vs <span>{{ awayTeam }}</span>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    homeTeam: {
+      type: String
+    },
+    awayTeam: {
+      type: String
+    },
+    clubData: {
+      type: Object
+    }
+  }
+}
+</script>
--- src/components/matches/Matches.vue
+++ src/components/matches/Matches.vue
@@ -1,11 +1,18 @@
 <template>
-  <div v-for="matchItem in matchData" :key="matchItem.id">
-    {{ matchItem.homeTeam.name }} vs {{ matchItem.awayTeam.name }}
-  </div>
+  <span class="match-item-container" v-for="matchItem in matchData" :key="matchItem.id">
+    <Match 
+      :homeTeam="matchItem.homeTeam.name"
+      :awayTeam="matchItem.awayTeam.name"
+    />
+  </span>
 </template>
 <script lang="ts">
+import Match from './Match.vue'
 
   export default {
+    components: {
+      Match
+    },
     props: {
       matchData: {
         type: Object
